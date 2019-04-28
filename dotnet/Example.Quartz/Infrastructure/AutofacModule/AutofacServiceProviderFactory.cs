0527df872c19aa46a548dc7c422583311b29daed
wmper <306632529@qq.com>
2019-04-28 10:48:52 +0800

    新增quartz示例

---
+++ dotnet/Example.Quartz/Infrastructure/AutofacModule/AutofacServiceProviderFactory.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.Quartz;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Specialized;
+
+namespace Example.Quartz.Infrastructure.AutofacModule
+{
+    public class AutofacServiceProviderFactory : IServiceProviderFactory<ContainerBuilder>
+    {
+        public ContainerBuilder CreateBuilder(IServiceCollection services)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+            builder.RegisterModule(new DefaultModule());
+            builder.RegisterModule(new QuartzAutofacFactoryModule()
+            {
+                ConfigurationProvider = c =>
+                {
+                    var config = new NameValueCollection {
+                            { "quartz.scheduler.instanceName", "Example.Quartz" },
+                            { "quartz.threadPool.type", "Quartz.Simpl.SimpleThreadPool, Quartz" },
+                            { "quartz.threadPool.threadCount", "10" },
+                            { "quartz.jobStore.misfireThreshold", "60000" },
+                            { "quartz.plugin.jobInitializer.type" ,"Quartz.Plugin.Xml.XMLSchedulingDataProcessorPlugin, Quartz.Plugins" },
+                            { "quartz.plugin.jobInitializer.fileNames", "quartz_jobs.xml" },
+                            { "quartz.plugin.jobInitializer.failOnFileNotFound", "true" },
+                            { "quartz.plugin.jobInitializer.scanInterval" , "120" }
+                        };
+
+                    return config;
+                }
+            });
+            builder.RegisterModule(new QuartzAutofacJobsModule(typeof(Program).Assembly));
+
+            return builder;
+        }
+
+        public IServiceProvider CreateServiceProvider(ContainerBuilder containerBuilder)
+        {
+            var container = containerBuilder.Build();
+
+            return new AutofacServiceProvider(container);
+        }
+    }
+}
---
+++ dotnet/Example.Quartz/QuartzHostService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example.Quartz
+{
+    public class QuartzHostService : IHostedService
+    {
+        private readonly ILogger _log;
+        private readonly IScheduler _scheduler;
+
+        public QuartzHostService(ILogger<QuartzHostService> log, IScheduler scheduler)
+        {
+            _log = log;
+            _scheduler = scheduler;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _log.LogInformation("QuartzHostService Start...");
+
+            await _scheduler.Start(cancellationToken);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _log.LogInformation("QuartzHostService Shutdown.");
+
+            await _scheduler.Shutdown(cancellationToken);
+        }
+    }
+}
