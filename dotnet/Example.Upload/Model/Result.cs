4fcf6cb61702486de12ad400b10cecf553764770
wmper <306632529@qq.com>
2019-04-29 17:34:06 +0800

    新增上传文件示例

---
+++ dotnet/Example.Upload/Model/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example.Upload.Model
+{
+    public class Result
+    {
+        /// <summary>
+        /// 文件名
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// 远程url
+        /// </summary>
+        public string RemoteUrl { get; set; }
+
+        /// <summary>
+        /// 文件长度字节
+        /// </summary>
+        public long Length { get; set; }
+
+        /// <summary>
+        /// 结果
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ dotnet/Example.Upload/Utils/ConfigurationManager.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Example.Upload.Utils
+{
+    public class ConfigurationManager
+    {
+        public static IConfiguration Configuration { get; set; }
+    }
+}
