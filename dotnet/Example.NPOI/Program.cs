9dbf1b8c6e674dad188ed897970cff115830f576
wmper <306632529@qq.com>
2019-04-29 15:58:22 +0800

    完善导入导出

--- dotnet/Example.NPOI/Program.cs
+++ dotnet/Example.NPOI/Program.cs
@@ -1,37 +1,20 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NPOI.HSSF.UserModel;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
-using SDK.Infrastructure.Model;
+using SDK.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 
 namespace Example.NPOI
 {
     class Program
     {
-        class ExcelTitle
-        {
-            /// <summary>
-            /// 标题名
-            /// </summary>
-            public string Name { get; set; }
-            /// <summary>
-            /// 数据字段名
-            /// </summary>
-            public string Field { get; set; }
-            /// <summary>
-            /// 单元格类型
-            /// </summary>
-            public CellType CellType { get; set; } = CellType.String;
-            /// <summary>
-            /// 单元格对齐方式
-            /// </summary>
-            public HorizontalAlignment Alignment { get; set; } = HorizontalAlignment.Center;
-        }
-
-        class Data
+        public class Data
         {
             public string ProductName { get; set; }
             public string No { get; set; }
@@ -39,119 +22,202 @@ namespace Example.NPOI
             public int Qty { get; set; }
         }
 
-        class QuartzTask : CommonEntity
-        {
-            public string TaskName { get; set; }
-            public string ExcelTitle { get; set; }
-            public string Data { get; set; }
-            public string UploadFile { get; set; }
-            public string DownloadFile { get; set; }
-        }
-
         static void Main(string[] args)
         {
             Console.WriteLine("Starting...");
 
             var dir = $"{AppDomain.CurrentDomain.BaseDirectory}/temp/";
+
+            var headers = new List<ExcelHeader>() {
+                    new ExcelHeader() { Name = "商品名称",Field="ProductName", CellType = CellType.String,Alignment = HorizontalAlignment.Left },
+                    new ExcelHeader() { Name = "编号",Field="No", CellType = CellType.String },
+                    new ExcelHeader() { Name = "价格",Field="Price", CellType = CellType.Numeric },
+                    new ExcelHeader() { Name = "库存",Field="Qty", CellType = CellType.Numeric }
+                };
+
+            // data
+            //var temps = new List<Data>();
+            //for (var i = 0; i < 10000; i++)
+            //{
+            //    temps.Add(new Data() { ProductName = $"4可口可乐250ml*12瓶/箱-{i}", No = "9900009308", Price = 5, Qty = 1 });
+            //}
+
+            //var json = JsonConvert.SerializeObject(temps);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //CreateExcel(dir, headers, json);
+
+            var sheet = GetSheet("http://127.0.0.1:8887/eb80440cfc6e46bfaaaa87a92734dc4e.xlsx", EUploadType.Remote);
+
+            var list = GetSheetData<IList<Data>>(sheet, headers);
+            Console.WriteLine(list.Count);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"End.{stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static void CreateExcel(string dir, IList<ExcelHeader> headers, string jsonData)
+        {
             if (!Directory.Exists(dir))
             {
                 Directory.CreateDirectory(dir);
             }
 
-            var newFile = dir + $"{DateTime.Now.ToString("yyyymmddHHmmss")}.xlsx";
+            var newFile = dir + $"{Guid.NewGuid().ToN()}.xlsx";
+
+            IWorkbook workbook = new XSSFWorkbook();
+
+            var sheet1 = workbook.CreateSheet("Sheet1");
+
+            // title
+            var title = sheet1.CreateRow(0);
+
+            var styleCenter = workbook.CreateCellStyle();
+            styleCenter.Alignment = HorizontalAlignment.Center;
+
+            var styleLeft = workbook.CreateCellStyle();
+            styleLeft.Alignment = HorizontalAlignment.Left;
+
+            var styleRight = workbook.CreateCellStyle();
+            styleRight.Alignment = HorizontalAlignment.Right;
+
+            var i = 0;
+            foreach (var item in headers)
+            {
+                var cell = title.CreateCell(i, CellType.String);
+                cell.CellStyle = styleCenter;
+                cell.SetCellValue(item.Name);
+
+                i++;
+            }
+
+            var j = 1;
+            var datas = JsonConvert.DeserializeObject<IList<object>>(jsonData);
+            foreach (var item in datas)
+            {
+                var obj = JObject.Parse(item.ToString());
+                var row = sheet1.CreateRow(j);
+
+                for (var k = 0; k < headers.Count; k++)
+                {
+                    var cellType = headers[k].CellType;
+                    var algin = headers[k].Alignment;
+                    var field = headers[k].Field;
+
+                    var cell = row.CreateCell(k, cellType);
+
+                    switch (algin)
+                    {
+                        case HorizontalAlignment.Left:
+                            cell.CellStyle = styleLeft;
+                            break;
+                        case HorizontalAlignment.Right:
+                            cell.CellStyle = styleRight;
+                            break;
+                        default:
+                            cell.CellStyle = styleCenter;
+                            break;
+                    }
+
+                    switch (cellType)
+                    {
+                        case CellType.Numeric:
+                            cell.SetCellValue(double.Parse(obj[field].ToString()));
+                            break;
+                        default:
+                            cell.SetCellValue(obj[field].ToString());
+                            break;
+                    }
+                }
+
+                j++;
+            };
 
             using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
             {
-                IWorkbook workbook = new XSSFWorkbook();
-
-                var sheet1 = workbook.CreateSheet("Sheet1");
-
-                // title
-                var title = sheet1.CreateRow(0);
-
-                var titles = new List<ExcelTitle>() {
-                    new ExcelTitle() { Name = "商品名称",Field="ProductName", CellType = CellType.String,Alignment = HorizontalAlignment.Left },
-                    new ExcelTitle() { Name = "编号",Field="No", CellType = CellType.String },
-                    new ExcelTitle() { Name = "价格",Field="Price", CellType = CellType.Numeric },
-                    new ExcelTitle() { Name = "库存",Field="Qty", CellType = CellType.Numeric }
-                };
-
-                var styleCenter = workbook.CreateCellStyle();
-                styleCenter.Alignment = HorizontalAlignment.Center;
-
-                var styleLeft = workbook.CreateCellStyle();
-                styleLeft.Alignment = HorizontalAlignment.Left;
-
-                var styleRight = workbook.CreateCellStyle();
-                styleRight.Alignment = HorizontalAlignment.Right;
-
-                var i = 0;
-                titles.ForEach(item =>
-                {
-                    var cell = title.CreateCell(i, CellType.String);
-                    cell.CellStyle = styleCenter;
-                    cell.SetCellValue(item.Name);
-
-                    i++;
-                });
-
-                // data
-                var temp = new List<Data>() {
-                    new Data(){ ProductName="4可口可乐250ml*12瓶/箱",No="9900009308",Price=5,Qty=1},
-                    new Data(){ ProductName="4可口可乐250ml*12瓶/箱",No="9900009308",Price=6,Qty=2},
-                    new Data(){ ProductName="4可口可乐250ml*12瓶/箱",No="9900009308",Price=7,Qty=3},
-                    new Data(){ ProductName="4可口可乐250ml*12瓶/箱",No="9900009308",Price=888888.89m,Qty=444444444}
-                };
-
-                var json = JsonConvert.SerializeObject(temp);
-
-
-                var j = 1;
-                var datas = JsonConvert.DeserializeObject<List<object>>(json);
-                datas.ForEach(item =>
-                {
-                    var obj = JObject.Parse(item.ToString());
-                    var row = sheet1.CreateRow(j);
-
-                    for (var k = 0; k < titles.Count; k++)
-                    {
-                        var cellType = titles[k].CellType;
-                        var algin = titles[k].Alignment;
-                        var field = titles[k].Field;
-
-                        var cell = row.CreateCell(k, cellType);
-
-                        switch (algin)
-                        {
-                            case HorizontalAlignment.Left:
-                                cell.CellStyle = styleLeft;
-                                break;
-                            case HorizontalAlignment.Right:
-                                cell.CellStyle = styleRight;
-                                break;
-                            default:
-                                cell.CellStyle = styleCenter;
-                                break;
-                        }
-
-                        switch (cellType)
-                        {
-                            case CellType.Numeric:
-                                cell.SetCellValue(double.Parse(obj[field].ToString()));
-                                break;
-                            default:
-                                cell.SetCellValue(obj[field].ToString());
-                                break;
-                        }
-                    }
-
-                    j++;
-                });
-
                 workbook.Write(fs);
             }
+        }
+        private static string GetSheetJson(ISheet sheet, IList<ExcelHeader> headers, int skipHeaderRow = 1, int skipFlooterRow = 0)
+        {
+            var list = new List<Dictionary<string, string>>();
+            var rowCount = sheet.LastRowNum - skipFlooterRow;
 
-            Console.WriteLine("End.");
+            for (var i = skipHeaderRow; i <= rowCount; i++)
+            {
+                var dic = new Dictionary<string, string>();
+                var cells = sheet.GetRow(i).Cells;
+                var j = 0;
+
+                foreach (var item in headers)
+                {
+                    dic.Add(item.Field, cells[j].ToString());
+                    j++;
+                }
+
+                list.Add(dic);
+            }
+
+            return JsonConvert.SerializeObject(list);
+        }
+
+        private static T GetSheetData<T>(ISheet sheet, IList<ExcelHeader> headers, int skipHeaderRow = 1, int skipFlooterRow = 0)
+        {
+            var json = GetSheetJson(sheet, headers, skipHeaderRow, skipFlooterRow);
+
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private static ISheet GetSheet(string pathOrUrl, EUploadType eUploadType)
+        {
+            if (pathOrUrl.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(pathOrUrl));
+            }
+
+            if (!pathOrUrl.EndsWith("xlsx") && !pathOrUrl.EndsWith("xls"))
+            {
+                throw new ArgumentNullException("error excel file.");
+            }
+
+            var memoryStream = new MemoryStream();
+
+            if (eUploadType == EUploadType.Remote)
+            {
+                var client = new HttpClient();
+                using (var stream = client.GetStreamAsync(pathOrUrl).Result)
+                {
+                    stream.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                }
+            }
+            else
+            {
+                using (var fs = new FileStream(pathOrUrl, FileMode.Open, FileAccess.Read))
+                {
+                    fs.CopyTo(memoryStream);
+                    memoryStream.Seek(0, SeekOrigin.Begin);
+                }
+            }
+
+            ISheet sheet = null;
+
+            if (pathOrUrl.EndsWith("xlsx"))
+            {
+                var workBook = new XSSFWorkbook(memoryStream);
+                sheet = workBook.GetSheetAt(0);
+            }
+            else
+            {
+                // 2003版 excel
+                var workBook = new HSSFWorkbook(memoryStream);
+                sheet = workBook.GetSheetAt(0);
+            }
+
+            return sheet;
         }
     }
 }
