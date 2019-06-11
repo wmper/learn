bdc4a4202200ac84d52a3e83b20ca623836b6322
wmper <306632529@qq.com>
2019-06-11 17:15:12 +0800

    新增RabbitMQ项目

---
+++ dotnet/RabbitMQ.Consume/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Consume
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    // To read launchSettings.json environment
+                    configHost.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        configHost.AddCommandLine(args);
+                    }
+                })
+                .ConfigureAppConfiguration((hostContext, configApp) =>
+                {
+                    // 设置根目录
+                    configApp.SetBasePath(Directory.GetCurrentDirectory());
+
+                    // 根据环境变量读取配置
+                    configApp.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    configApp.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ConsumerHostedService>();
+                })
+                .ConfigureLogging((hostContext, configLogging) =>
+                {
+                    configLogging.AddConsole();
+                    configLogging.AddDebug();
+                })
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
