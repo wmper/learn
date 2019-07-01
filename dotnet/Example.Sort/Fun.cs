2e4c5d93aadb8a7e2d82b140be8e654a7b28ec69
wmper <306632529@qq.com>
2019-07-01 10:55:16 +0800

    新增排序算法

---
+++ dotnet/Example.Sort/Fun.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Example.Sort
+{
+    public class Fun
+    {
+        /// <summary>
+        /// 冒泡排序
+        /// </summary>
+        /// <param name="arr"></param>
+        public static int[] Sort(int[] arr)
+        {
+            // 3, 6, 9, 1, 4, 0, 7, 2, 5, 8 
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    int temp = 0;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        public static int[] SelectSort(int[] arr)
+        {
+            // 3, 6, 9, 1, 4, 0, 7, 2, 5, 8 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min])
+                    {
+                        min = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[min];
+                arr[min] = temp;
+            }
+
+            return arr;
+        }
+
+        public static int[] InsertSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 1; i < n; ++i)
+            {
+                int value = arr[i];
+                int j = 0;
+                for (j = i - 1; j >= 0; j--)
+                {
+                    if (arr[j] > value)
+                    {
+                        arr[j + 1] = arr[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                arr[j + 1] = value;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ dotnet/Example.Sort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Example.Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var input = new[] { 3, 6, 9, 1, 4, 0, 7, 2, 5, 8 };
+
+            //var arr = Fun.Sort(input);
+            //var arr = Fun.SelectSort(input);
+            var arr = Fun.InsertSort(input);
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
