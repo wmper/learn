f51f34ee3a8722814ff0744b6d852d484c874e7d
wmper <306632529@qq.com>
2019-07-17 11:27:16 +0800

    服务示例

---
+++ dotnet/Example.Consul.ServiceA/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Example.Consul.ServiceA.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ServiceA" };
+        }
+    }
+}
---
+++ dotnet/Example.Consul.ServiceB/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Example.Consul.ServiceB.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ServiceB" };
+        }
+    }
+}
