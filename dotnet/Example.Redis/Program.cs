6b7f9ec87d8b62d3ab0bec15838239ad1858ec03
wmper <306632529@qq.com>
2019-09-03 10:38:50 +0800

    新增redis测试

---
+++ dotnet/Example.Redis/Program.cs
@@ -0,0 +1,46 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example.Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.186.73:6379");
+            IDatabase db = redis.GetDatabase();
+
+            string value = "abcdefg";
+            db.StringSet("mykey", value);
+
+            string result = db.StringGet("mykey");
+            Console.WriteLine(result);
+
+            var values = new List<HashEntry> { };
+
+            for (var i = 0; i <= 10; i++)
+            {
+                HashEntry entry = new HashEntry("k_" + i, "v_" + i);
+                values.Add(entry);
+            }
+
+            db.HashSet("hash", values.ToArray());
+
+            var hResult = db.HashGet("hash", "k_5");
+            Console.WriteLine(hResult);
+
+
+            db.ListLeftPush("list", new RedisValue[] { 1, 2, 3, 4 });
+            Console.WriteLine(db.ListRightPop("list"));
+
+            var j = db.StringIncrement("auto");
+            Console.WriteLine(j);
+
+            Console.Read();
+        }
+    }
+}
--- dotnet/Example/Program.cs
+++ dotnet/Example/Program.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Example
 {
@@ -9,24 +11,75 @@ namespace Example
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("thread-1:" + Thread.CurrentThread.ManagedThreadId);
+            //Console.WriteLine("thread-1:" + Thread.CurrentThread.ManagedThreadId);
 
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
 
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    await Test(i);
-            //}
+            for (int i = 0; i < 20; i++)
+            {
+                Test2();
+                i++;
+            }
 
-            await TestAsync(0);
+            //await TestAsync(0);
 
             stopwatch.Stop();
 
             Console.WriteLine("time:" + stopwatch.ElapsedMilliseconds + "ms");
 
+            //var model = new YieldTest()
+            //{
+            //    Name = "小明",
+            //    Age = 18,
+            //    Province = "广东",
+            //    City = "广州",
+            //    Country = "天河",
+            //    Address = "银汇大厦"
+            //};
+
+            //foreach (var item in model.GetAtomicValues())
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //string str = "adfdafdfasdfaf";
+
+
+
+            //Console.WriteLine(str[2]);
+
+            //var w16 = 0x2A;
+            //var w2 = 0b_0010_1010;
+
+            //Console.WriteLine(w16);
+            //Console.WriteLine(w2);
 
             Console.Read();
+
+        }
+
+        enum Day : byte { Sat = 1, Sun, Mon, Tue, Wed, Thu, Fri };
+
+        class YieldTest
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Province { get; set; }
+            public string City { get; set; }
+            public string Country { get; set; }
+            public string Address { get; set; }
+
+            public IEnumerable<object> GetAtomicValues()
+            {
+                yield return Name;
+                yield return Age;
+                yield return Province;
+                yield return City;
+                yield return Country;
+                yield return Address;
+            }
         }
 
         static async Task TestAsync(int i)
@@ -40,21 +93,23 @@ namespace Example
 
             //Console.WriteLine(i + ":" + Thread.CurrentThread.ManagedThreadId);
 
-            await Test2();
+            //await Test2();
 
             Console.WriteLine("thread-5:" + Thread.CurrentThread.ManagedThreadId);
         }
 
-        static Task Test2()
+        static void Test2()
         {
             Console.WriteLine("thread-2:" + Thread.CurrentThread.ManagedThreadId);
 
-            return Task.Factory.StartNew(() =>
+            var task = Task.Factory.StartNew(() =>
             {
                 Thread.Sleep(2000);
 
                 Console.WriteLine("thread-3:" + Thread.CurrentThread.ManagedThreadId);
             });
+
+            task.Wait();
         }
     }
 }
