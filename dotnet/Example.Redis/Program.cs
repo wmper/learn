bb414e25f108d3e8ff46c90217bb3a8bbecf4bbb
wmper <306632529@qq.com>
2019-09-04 17:52:37 +0800

    测试 redis方法

--- dotnet/Example.Redis/Program.cs
+++ dotnet/Example.Redis/Program.cs
@@ -9,37 +9,70 @@ namespace Example.Redis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!" + DateTime.MinValue);
 
             ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.186.73:6379");
+
             IDatabase db = redis.GetDatabase();
 
-            string value = "abcdefg";
-            db.StringSet("mykey", value);
+            //string value = "abcdefg";
+            //db.StringSet("mykey", value);
 
-            string result = db.StringGet("mykey");
-            Console.WriteLine(result);
+            //Console.WriteLine(db.StringGet("mykey"));
 
-            var values = new List<HashEntry> { };
+            //db.StringAppend("mykey", "append");
 
-            for (var i = 0; i <= 10; i++)
-            {
-                HashEntry entry = new HashEntry("k_" + i, "v_" + i);
-                values.Add(entry);
-            }
+            //Console.WriteLine(db.StringGet("mykey"));
 
-            db.HashSet("hash", values.ToArray());
+            //db.StringSetRange("mykey", 5, "range");
 
-            var hResult = db.HashGet("hash", "k_5");
-            Console.WriteLine(hResult);
+            //Console.WriteLine(db.StringGet("mykey"));
 
+            //var values = new List<HashEntry> { };
+
+            //for (var i = 0; i <= 5; i++)
+            //{
+            //    HashEntry entry = new HashEntry("k_" + i, "vv_" + i);
+            //    values.Add(entry);
+            //}
+
+            //db.HashSet("hash", values.ToArray());
+
+            //var hResult = db.HashGet("hash", "k_5");
+            //Console.WriteLine(hResult);
+
+            //var list = db.HashGetAll("hash");
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item.Value);
+            //}
+
+            //db.HashIncrement("hashset", "index");
 
             db.ListLeftPush("list", new RedisValue[] { 1, 2, 3, 4 });
             Console.WriteLine(db.ListRightPop("list"));
 
-            var j = db.StringIncrement("auto");
-            Console.WriteLine(j);
+            //var j = db.StringIncrement("auto");
+            //Console.WriteLine(j);
 
+            //var tran = db.CreateTransaction();
+            //tran.StringSetAsync("tran", "123456", TimeSpan.FromMinutes(1));
+            //tran.StringSetAsync("tran2", "1234562", TimeSpan.FromMinutes(2));
+            //tran.Execute();
+
+            //db.SortedSetAdd("sort", "score1", 1.0);
+            //db.SortedSetAdd("sort", "score3", 3.0);
+            //db.SortedSetAdd("sort", "score2", 2.0);
+
+            //Console.WriteLine(db.KeyRandom());
+
+            //var list = db.SortedSetRangeByRank("sort", 0, 3, Order.Descending);
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            Console.WriteLine("the end.");
             Console.Read();
         }
     }
