e91c0be808261a400689e1aaee524f4faa901668
wmper <306632529@qq.com>
2019-09-05 15:31:53 +0800

    62短码

---
+++ dotnet/Example.Bit62/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Example.Bit62
+{
+    class Program
+    {
+        public static string EncodeStr(long num)
+        {
+            int scale = 62;
+            StringBuilder sb = new StringBuilder();
+            char[] charArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            long remainder = 0;
+
+            do
+            {
+                remainder = num % scale;
+                sb.Append(charArray[remainder]);
+                num = num / scale;
+            }
+            while (num > scale - 1);
+
+            sb.Append(charArray[num]);
+
+            char[] chars = sb.ToString().ToCharArray();
+            Array.Reverse(chars);
+            string result = new string(chars);
+
+            // 6 bits are required, add leading zeros when the encoded string < 6 long
+            return result.PadLeft(6, '0');
+        }
+
+        public static long DecodeNum(string str)
+        {
+            int scale = 62;
+            string charArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            // Trim the leading zeros first            
+            str = Regex.Replace(str, "^0*", "");
+
+            long num = 0;
+            int index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                index = charArray.IndexOf(str[i]);
+                num += (long)(index * (Math.Pow(scale, str.Length - i - 1)));
+            }
+
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
