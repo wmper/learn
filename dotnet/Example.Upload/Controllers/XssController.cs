830b88ff2443fe90bc99d2befac311842a62803f
wmper <306632529@qq.com>
2019-09-17 11:25:56 +0800

    xss过滤特性

---
+++ dotnet/Example.Upload/Controllers/XssController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Example.Upload.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Example.Upload.Controllers
+{
+    [Route("api/[controller]")]
+    public class XssController : Controller
+    {
+        [HttpPost]
+        [XSS]
+        public string Post([FromBody] Test value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+
+    public class Test
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Second Second { get; set; }
+    }
+
+    public class Second
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ dotnet/Example.Upload/Filters/XSSAttribute.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Example.Upload.Filters
+{
+    public class XSSAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var paramters = context.ActionDescriptor.Parameters;
+            foreach (var paramter in paramters)
+            {
+                if (context.ActionArguments[paramter.Name] == null) continue;
+
+                if (paramter.ParameterType == typeof(string))
+                {
+                    var value = context.ActionArguments[paramter.Name].ToString();
+
+                    // Filter XSS
+                    value = Regex.Replace(value, @"<[^>]*>", string.Empty);
+
+                    context.ActionArguments[paramter.Name] = value;
+                }
+                else if (paramter.ParameterType.IsClass)
+                {
+                    PostModelProcess(paramter.ParameterType, context.ActionArguments[paramter.Name]);
+                }
+            }
+        }
+
+        private static object PostModelProcess(Type type, object obj)
+        {
+            if (obj == null) return obj;
+
+            var properties = type.GetProperties();
+            foreach (var property in properties)
+            {
+                if (property.GetValue(obj) == null) continue;
+
+                if (property.PropertyType == typeof(string))
+                {
+                    var value = property.GetValue(obj).ToString();
+
+                    // Filter XSS
+                    value = Regex.Replace(value, @"<[^>]*>", string.Empty);
+
+                    property.SetValue(obj, value);
+                }
+                else if (property.PropertyType.IsClass)
+                {
+                    property.SetValue(obj, PostModelProcess(property.PropertyType, property.GetValue(obj)));
+                }
+            }
+
+            return obj;
+        }
+    }
+}
