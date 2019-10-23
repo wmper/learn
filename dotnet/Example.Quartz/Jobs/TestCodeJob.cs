2ac7b9db21b537a391f89d25a2dce908b9fe8f03
wmper <306632529@qq.com>
2019-10-23 11:31:16 +0800

    新增代码式job

---
+++ dotnet/Example.Quartz/Jobs/TestCodeJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace Example.Quartz.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class TestCodeJob : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("coding...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ dotnet/Example.Quartz/Jobs/TestCodeTrigger.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Quartz;
+using SDK.Infrastructure.Interface;
+
+namespace Example.Quartz.Jobs
+{
+    public class TestCodeTrigger //: IStartup
+    {
+        public void Initialize(ILifetimeScope container, params string[] assemblyStringPattens)
+        {
+            var scheduler = container.Resolve<IScheduler>();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<TestCodeJob>()
+                .WithIdentity("job1", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then repeat every 10 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
+                .Build();
+
+            // Tell quartz to schedule the job using our trigger
+            scheduler.ScheduleJob(job, trigger).GetAwaiter().GetResult();
+        }
+    }
+}
